a39dee5f61d9d596f586ef5b900d506be7fba6fe
jamesz-007 <jivan15933@gmail.com>
2026-09-04 14:46:32 +0530

    Implement Game Layer
     - Initialize Game Layer files
     - Define basic Game class

---
+++ app/game/featured/M-mp-lvl_1/game.js
@@ -0,0 +1,53 @@
+const Game = (async () => {
+
+    const request = await fetch('./game.json');
+    const gameInfo = await request.json();
+
+    /**
+     * Loads the game into gameFrame element
+     * @param {HTMLIFrameElement} gameFrame 
+     */
+    function load(gameFrame) {
+        gameFrame.src = "/game/featured/M-mp-lvl_1/source/index.html";
+
+        gameFrame.contentWindow.postMessage({
+
+        }, "*");
+
+        
+    }
+
+    function _getStats() {
+        
+    }
+
+    function getResponse() {
+        const { profile } = gameInfo;
+        const { mistakes, moves } = _getStats();
+        const accuracy = _calculateAccuracy(mistakes, moves);
+
+        const memory = accuracy * profile.get("memory");
+
+        const timeExp = gameInfo.timeExp;
+        const timeTaken = _getStats().time;
+
+        const focus = accuracy * (timeExp / timeTaken) * profile.get("focus");
+
+        return {
+            memory,
+            focus
+        };
+    }
+
+    function _calculateAccuracy(mistakes, total) {
+        return (1 - (mistakes / total));
+    }
+
+    return {
+        load,
+        getResponse
+    };
+
+})();
+
+export default Game;
